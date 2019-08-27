db69ae86c551f5ec6e7cd40a7c891e78b756fc15
gghatano <adad0405@gmail.com>
2019-08-27 18:12:50 +0900

    ADD progress bar

--- server/server_round1_anon_reid.R
+++ server/server_round1_anon_reid.R
@@ -1,48 +1,58 @@
+##################################
+## ID再識別対応匿名加工後データ　評価タブ
+
+SLEEP_TIME = 2
+
 output$table_round1_anon_reid = renderDataTable({
-  ## ファイル読み込み
-  file_round1_anon_reid = input$file_round1_anon_reid
-  if(is.null(file_round1_anon_reid)){
-    return(NULL)
-  }
   
-  
-  ## データ読み込み
-  table_round1_anon_reid = readr::read_csv(file = file_round1_anon_reid$datapath)
-  
-  if(is.null(table_round1_anon_reid)){
-    return(NULL)
-  }
-  
-  
-  ## チェック中、と表示する
-  output$format_round1_anon_reid = renderText({
-    return("チェック中...")
-  })
-  
-  ## 計算中、と表示する
-  output$utility_round1_anon_reid = renderText({
-    return("計算中...")
-  })
-  
-  ## formatのチェック処理
-  output$format_round1_anon_reid = renderText({
-    result = anon_formatcheck(table_round1_anon_reid)
-    result %>% return
-  })
-  
-  ## utility check
-  output$utility_round1_anon_reid = renderText({
+  withProgress(message = '処理中...', value = 0, {
     
-    utility_score = utility(rawdata = table_round1_anon_reid, anondata = table_round1_anon_reid)
+    ## ファイル読み込み
+    Sys.sleep(SLEEP_TIME)
+    incProgress(1/4, detail = "ファイル読み込み中...")
     
-    ## TODO :: ファイル名と有用性評価値と投稿日時をアップロードする仕組みを作る
+    file_round1_anon_reid = input$file_round1_anon_reid
+    if(is.null(file_round1_anon_reid)){
+      return(NULL)
+    }
     
-    return(utility_score)
+    ## データ読み込み
+    table_round1_anon_reid = readr::read_csv(file = file_round1_anon_reid$datapath)
+    
+    if(is.null(table_round1_anon_reid)){
+      return(NULL)
+    }
+    
+    ## formatのチェック処理
+    Sys.sleep(SLEEP_TIME)
+    incProgress(2/4, detail = "フォーマットチェック中...")
+    
+    output$format_round1_anon_reid = renderText({
+      result = anon_formatcheck(table_round1_anon_reid)
+      result %>% return
+    })
+    
+    ## utility check
+    Sys.sleep(SLEEP_TIME)
+    incProgress(3/4, detail = "有用性計算中...")
+    
+    output$utility_round1_anon_reid = renderText({
+      
+      utility_score = utility(rawdata = table_round1_anon_reid, anondata = table_round1_anon_reid)
+      
+      ## TODO :: ファイル名と有用性評価値と投稿日時をアップロードする仕組みを作る
+      
+      return(utility_score)
+      
+    })
+    
+    ## 投稿済みデータの表示
+    
+    Sys.sleep(SLEEP_TIME)
+    incProgress(4/4, detail = "データ表示中...")
+    table_round1_anon_reid %>% return
     
   })
-  
-  ## 投稿済みデータの示
-  table_round1_anon_reid %>% return
 }, options = list(pageLength = 10))
 
 output$format_round1_anon_reid = renderText({
